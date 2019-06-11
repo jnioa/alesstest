8ea7ae5292b1081222bdd7e8351213da3af5125d
jnioa <last14569@ezweb.ne.jp>
2019-06-11 12:49:51 +0900

    5

--- taiki/views.py
+++ taiki/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     str_out = ""
-    str_out += "3分後にリンクをクリックしてください。<p />"
+    str_out += "5分後にリンクをクリックしてください。<p />"
     str_out += "<a href='https://docs.google.com/forms/d/1CoUGUxYTln1OyMcJD1Ty6uEi7nC-UIaDjzxdOV6ig1E/viewform?pli=1&edit_requested=true'>start</a><p />"
     return HttpResponse(str_out)
--- taiki2/views.py
+++ taiki2/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     str_out = ""
-    str_out += "3分後にリンクをクリックしてください。<p />"
+    str_out += "5分後にリンクをクリックしてください。<p />"
     str_out += "<a href='https://docs.google.com/forms/d/1MEbvCvjGDmpgnrPuqpvktjqwNaIl904yC44GzFeSXZY/viewform?edit_requested=true'>start</a><p />"
     return HttpResponse(str_out)
